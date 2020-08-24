554759c51fa84a8059ad2eabda0fd70eadbc7d82
liwenjing <15119285499@163.com>
2020-08-24 18:35:38 +0800

    .

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="hello">
     <div class="vfd-container">
-      <vfd ref="vfd" />
+      <vfd ref="vfd" :style="vfdHeight" />
     </div>
+    <!-- <vfd /> -->
   </div>
 </template>
 
@@ -15,30 +16,104 @@ export default {
     vfd
   },
   mounted() {
-    let tData = JSON.parse(this.timeLineData);
-    tData.map((item, index) => {
+    // // 时间轴数据
+    // let tData = JSON.parse(this.timeLineData);
+    // // 添加节点
+    // tData.map((item, index) => {
+    //   this.showData.nodeList.push({
+    //     type: "child-flow",
+    //     nodeName: item.statusType,
+    //     icon: "ChildFlowIcon",
+    //     id: String(item.id),
+    //     height: 50,
+    //     x: 100,
+    //     width: 50,
+    //     y: 200 + index * 100,
+    //     status: item.status,
+    //     background: '#67C23A'
+    //   });
+    // });
+    // // 添加节点连接线
+    // this.showData.nodeList.map((item, index) => {
+    //   if (this.showData.nodeList[index + 1]) {
+    //     this.showData.linkList.push({
+    //       type: "link",
+    //       sourceId: String(this.showData.nodeList[index].id),
+    //       targetId: String(this.showData.nodeList[index + 1].id),
+    //       cls: {
+    //         linkType: "Flowchart",
+    //         linkColor: "#2a2929"
+    //       }
+    //     });
+    //   }
+    // });
+
+    // this.vfdHeight =  `height: ${(this.showData.nodeList.length + 2) * 100 + 200}px`
+
+    // // 新增 开始节点
+    // this.showData.nodeList.unshift({
+    //   type: "start",
+    //   nodeName: "开始",
+    //   icon: "StartIcon",
+    //   id: "start",
+    //   height: 50,
+    //   x: 135,
+    //   width: 50,
+    //   y: 100
+    // });
+    // let startLine = JSON.parse(JSON.stringify(this.showData.linkList[0]))
+    // startLine.targetId = startLine.sourceId
+    // startLine.sourceId = 'start'
+    // this.showData.linkList.unshift(startLine)
+
+    // // 新增 结束节点
+    // this.showData.nodeList.push({
+    //   type: "end",
+    //   nodeName: "结束",
+    //   icon: "EndIcon",
+    //   id: "end",
+    //   height: 50,
+    //   x: 135,
+    //   width: 50,
+    //   y: 100 + this.showData.nodeList.length * 100
+    // });
+    // let endLine = JSON.parse(JSON.stringify(this.showData.linkList[this.showData.linkList.length-1]))
+    // endLine.sourceId = endLine.targetId
+    // endLine.targetId = 'end'
+    // this.showData.linkList.push(endLine)
+
+    // // 新增 节点分支
+    // let dataArr = [];
+    // this.showData.nodeList.map((item, index) => {
+    //   if (item.status === 2) {
+    //     let data = JSON.parse(JSON.stringify(item));
+    //     data.id = `-${item.id}`;
+    //     data.nodeName = "审核不成功";
+    //     data.x = 250;
+    //     data.background = ''
+    //     dataArr.push(data);
+    //   }
+    // });
+    // this.showData.nodeList = [...this.showData.nodeList, ...dataArr];
+
+    // console.log(this.showData);
+    // this.$refs.vfd.loadFlow(JSON.stringify(this.showData));
+
+    this.stateContainer.stateData.map((item, index) => {
       this.showData.nodeList.push({
         type: "child-flow",
-        nodeName: item.statusType,
+        nodeName: item.statusTitle,
         icon: "ChildFlowIcon",
         id: String(item.id),
         height: 50,
         x: 100,
         width: 50,
-        y: 100 + index * 100
+        y: 200 + index * 100,
+        status: item.statusId,
+        background: '#67C23A'
       });
-      if (tData[index + 1]) {
-        this.showData.linkList.push({
-          type: "link",
-          sourceId: String(tData[index].id),
-          targetId: String(tData[index+1].id),
-          cls: {
-            linkType: "Flowchart",
-            linkColor: "#2a2929"
-          }
-        });
-      }
     });
+
     this.$refs.vfd.loadFlow(JSON.stringify(this.showData));
   },
   data() {
@@ -51,7 +126,7 @@ export default {
           id: ""
         },
         config: {
-          showGrid: false,
+          showGrid: true,
           showGridText: "显示网格",
           showGridIcon: "eye"
         },
@@ -59,7 +134,33 @@ export default {
         remarks: []
       },
       timeLineData:
-        '[{"id":45,"name":"大叶凤尾","reviewer_name":"admin","status":2,"remark":"","create_time":"2020-08-20 13:38:17","statusType":"审核成功"},{"id":44,"name":"大叶凤尾","reviewer_name":"admin","status":2,"remark":"","create_time":"2020-08-20 13:38:09","statusType":"审核成功"},{"id":43,"name":"大叶凤尾","reviewer_name":"admin","status":1,"remark":"","create_time":"2020-08-20 13:38:00","statusType":"提交审核"},{"id":42,"name":"大叶凤尾","reviewer_name":"admin","status":4,"remark":"1","create_time":"2020-08-20 13:37:49","statusType":"已驳回"}]'
+        '[{"id":45,"name":"大叶凤尾","reviewer_name":"admin","status":2,"remark":"","create_time":"2020-08-20 13:38:17","statusType":"审核成功"},{"id":44,"name":"大叶凤尾","reviewer_name":"admin","status":2,"remark":"","create_time":"2020-08-20 13:38:09","statusType":"审核成功"},{"id":43,"name":"大叶凤尾","reviewer_name":"admin","status":1,"remark":"","create_time":"2020-08-20 13:38:00","statusType":"提交审核"},{"id":42,"name":"大叶凤尾","reviewer_name":"admin","status":4,"remark":"1","create_time":"2020-08-20 13:37:49","statusType":"已驳回"}]',
+      vfdHeight: 'height: 1000px;',
+      stateContainer: {
+        doubleCheck: [],
+        stateData: [
+          {
+            statusId: 1,
+            statusTitle: '未审核'
+          },
+          {
+            statusId: 2,
+            statusTitle: '已审核'
+          },
+          {
+            statusId: 3,
+            statusTitle: '已复核'
+          },
+          {
+            stateId: 4,
+            statusTitle: '已审核'
+          },
+          {
+            statusId: 5,
+            statusTitle: '已驳回'
+          }
+        ]
+      }
     };
   }
 };
@@ -82,7 +183,7 @@ export default {
     }
     /deep/ .ant-layout {
       width: 100%;
-      height: 1500px;
+      // height: 1500px;
       // .ant-layout-header {
       //   display: none;
       // }
